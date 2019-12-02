d07be62217d1aa2c02008b47e53cbe3fe104ad07
Shepherd <qizhen816@163.com>
2019-12-02 16:05:52 +0800

    Add configurations
    Configurations for new augmentation methods and several loss weights

--- maskrcnn_benchmark/config/defaults.py
+++ maskrcnn_benchmark/config/defaults.py
@@ -26,7 +26,11 @@ _C.MODEL.MASK_ON = False
 _C.MODEL.MASKIOU_ON = False
 _C.MODEL.DEVICE = "cuda"
 _C.MODEL.META_ARCHITECTURE = "GeneralizedRCNN"
+_C.MODEL.MASK_LOSS_WEIGHT = 10.0
 _C.MODEL.MASKIOU_LOSS_WEIGHT = 1.0
+_C.MODEL.BOX_LOSS_WEIGHT = 10.0
+_C.MODEL.CLASSIFICATION_LOSS_WEIGHT = 2.0
+
 # If the WEIGHT starts with a catalog://, like :R-50, the code will look for
 # the path in paths_catalog. Else, it will use it as the specified absolute
 # path
@@ -38,13 +42,13 @@ _C.MODEL.PRETRAINED_MODELS = "pretrained_models"
 # -----------------------------------------------------------------------------
 _C.INPUT = CN()
 # Size of the smallest side of the image during training
-_C.INPUT.MIN_SIZE_TRAIN = 800  # (800,)
+_C.INPUT.MIN_SIZE_TRAIN = 800
 # Maximum size of the side of the image during training
-_C.INPUT.MAX_SIZE_TRAIN = 1333
+_C.INPUT.MAX_SIZE_TRAIN = 800 #1333
 # Size of the smallest side of the image during testing
 _C.INPUT.MIN_SIZE_TEST = 800
 # Maximum size of the side of the image during testing
-_C.INPUT.MAX_SIZE_TEST = 1333
+_C.INPUT.MAX_SIZE_TEST = 800 #1333
 # Values to be used for image normalization
 _C.INPUT.PIXEL_MEAN = [102.9801, 115.9465, 122.7717]
 # Values to be used for image normalization
@@ -52,6 +56,24 @@ _C.INPUT.PIXEL_STD = [1., 1., 1.]
 # Convert image to BGR format (for Caffe2 models), in range 0-255
 _C.INPUT.TO_BGR255 = True
 
+# Image ColorJitter
+_C.INPUT.BRIGHTNESS = 0.5#0.3
+_C.INPUT.CONTRAST = 0.5#0.3
+_C.INPUT.SATURATION = 0.5#0.2
+_C.INPUT.HUE = 0.3#0.1
+
+# Flips
+_C.INPUT.HORIZONTAL_FLIP_PROB_TRAIN = 0.5
+_C.INPUT.VERTICAL_FLIP_PROB_TRAIN = 0.5
+
+# Rotate
+_C.INPUT.ANGLE_TRAIN = 15#10
+
+# Linear Light Shade
+_C.INPUT.VERTICAL_LIGHT_PROB_TRAIN = 0.5
+_C.INPUT.VERTICAL_LIGHT_SCALE_TRAIN = 20
+_C.INPUT.HORIZONTAL_LIGHT_PROB_TRAIN = 0.5
+_C.INPUT.HORIZONTAL_LIGHT_SCALE_TRAIN = 20
 
 # -----------------------------------------------------------------------------
 # Dataset
@@ -180,7 +202,7 @@ _C.MODEL.ROI_BOX_HEAD.PREDICTOR = "FastRCNNPredictor"
 _C.MODEL.ROI_BOX_HEAD.POOLER_RESOLUTION = 14
 _C.MODEL.ROI_BOX_HEAD.POOLER_SAMPLING_RATIO = 0
 _C.MODEL.ROI_BOX_HEAD.POOLER_SCALES = (1.0 / 16,)
-_C.MODEL.ROI_BOX_HEAD.NUM_CLASSES = 81
+_C.MODEL.ROI_BOX_HEAD.NUM_CLASSES = 11#81
 # Hidden layer dimension when using an MLP for the RoI box head
 _C.MODEL.ROI_BOX_HEAD.MLP_HEAD_DIM = 1024
 
@@ -233,22 +255,22 @@ _C.MODEL.RESNETS.STEM_OUT_CHANNELS = 64
 _C.SOLVER = CN()
 _C.SOLVER.MAX_ITER = 40000
 
-_C.SOLVER.BASE_LR = 0.001
+_C.SOLVER.BASE_LR = 0.0001#0.001
 _C.SOLVER.BIAS_LR_FACTOR = 2
 
 _C.SOLVER.MOMENTUM = 0.9
 
-_C.SOLVER.WEIGHT_DECAY = 0.0005
+_C.SOLVER.WEIGHT_DECAY = 0.0001#0.0005
 _C.SOLVER.WEIGHT_DECAY_BIAS = 0
 
 _C.SOLVER.GAMMA = 0.1
-_C.SOLVER.STEPS = (30000,)
+_C.SOLVER.STEPS = (3000,) #(30000,)
 
 _C.SOLVER.WARMUP_FACTOR = 1.0 / 3
-_C.SOLVER.WARMUP_ITERS = 500
+_C.SOLVER.WARMUP_ITERS = 0#500
 _C.SOLVER.WARMUP_METHOD = "linear"
 
-_C.SOLVER.CHECKPOINT_PERIOD = 10000
+_C.SOLVER.CHECKPOINT_PERIOD = 5000
 
 # Number of images per batch
 # This is global, so if we have 8 GPUs and IMS_PER_BATCH = 16, each GPU will
